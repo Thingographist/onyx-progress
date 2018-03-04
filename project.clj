2f0907d9326caa047eec39f70a81e7ae1a20a990
khoda <elizavetaskull@gmail.com>
2018-03-04 11:19:53 +0700

    update zoo-routing version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-progress "0.1.0"
+(defproject onyx-progress "0.1.1"
   :description "Progress monitoring through Zookeeper"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -11,7 +11,7 @@
                  [org.apache.curator/curator-framework "4.0.0" :exclusions [org.slf4j/slf4j-api
                                                                             org.apache.zookeeper/zookeeper]]
                  [org.slf4j/slf4j-api "1.7.25"]
-                 [zoo-routing "0.1.2" :exclusions [log4j
+                 [zoo-routing "0.1.3" :exclusions [log4j
                                                    org.clojure/tools.reader
                                                    org.apache.zookeeper/zookeeper
                                                    org.clojure/core.async]]
